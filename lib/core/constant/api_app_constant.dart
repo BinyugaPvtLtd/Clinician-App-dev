6b9a31baa5fecc215840389aa9d5f191d4fdcc33
ShubhDev <shubham.dev@symmetry.care>
2026-04-28 16:55:59 +0530

    demo domain

--- lib/core/constant/api_app_constant.dart
+++ lib/core/constant/api_app_constant.dart
@@ -1,5 +1,5 @@
 class ApiAppConstant {
-  static const String domain = 'https://dev.symmetry.care/';
+  static const String domain = 'https://demo.symmetry.care/';
   static const String authDomain = 'https://auth.symmetry.care/';
   static const String clinicialBaseUrl = '${domain}api/';
   static const String clinicialAuthBaseUrl = '${authDomain}api/';
