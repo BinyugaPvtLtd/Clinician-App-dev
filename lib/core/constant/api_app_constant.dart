2697cf56afa3db8c5b8fc50a12ccb7fd28b26fc6
prajwalbin <prajwallok8@gmail.com>
2026-06-30 14:30:22 +0530

    new branch push.. fix validator and push new branch  completed edit functionality and hide add button  in timesheet tab push code prajwal_dev_29_06 give dev build after  give demo build

--- lib/core/constant/api_app_constant.dart
+++ lib/core/constant/api_app_constant.dart
@@ -1,5 +1,34 @@
+///dev
+// class ApiAppConstant {
+//   static  String domain = 'https://dev.symmetry.care/';
+//   static const String authDomain = 'https://auth.symmetry.care/';
+//   static  String clinicialBaseUrl = '${domain}api/';
+//   static const String clinicialAuthBaseUrl = '${authDomain}api/';
+//   static const googleApiKey = "AIzaSyCw6mXOPCtbKn5i0bWcAcnfXCkb0y5G7Lg";
+//
+//   static const int proHealth = 2;
+//   static const int clinician = 1;
+//   //  pass appType to get dynamic base URL
+//   static String endPointByAlias(int appType,String aliasString) {
+//     switch (appType) {
+//       case clinician:
+//         domain = "https://dev.symmetry.care";
+//         return "https://dev.symmetry.care";
+//       case proHealth:
+//         domain = "https://dev.symmetry.care";
+//         return "https://dev.symmetry.care";
+//       default:
+//         domain = "https://dev.symmetry.care/";
+//         return "https://dev.symmetry.care/";
+//     }
+//   }
+// }
+
+
+
+///demo
 class ApiAppConstant {
-  static  String domain = 'https://dev.symmetry.care/';
+  static  String domain = 'https://demo.symmetry.care/';
   static const String authDomain = 'https://auth.symmetry.care/';
   static  String clinicialBaseUrl = '${domain}api/';
   static const String clinicialAuthBaseUrl = '${authDomain}api/';
@@ -11,14 +40,14 @@ class ApiAppConstant {
   static String endPointByAlias(int appType,String aliasString) {
     switch (appType) {
       case clinician:
-        domain = "https://dev.symmetry.care";
-        return "https://dev.symmetry.care";
+        domain = "https://demo.symmetry.care";
+        return "https://demo.symmetry.care";
       case proHealth:
-        domain = "https://dev.symmetry.care";
-        return "https://dev.symmetry.care";
+        domain = "https://demo.symmetry.care";
+        return "https://demo.symmetry.care";
       default:
-        domain = "https://dev.symmetry.care/";
-        return "https://dev.symmetry.care/";
+        domain = "https://demo.symmetry.care/";
+        return "https://demo.symmetry.care/";
     }
   }
 }
