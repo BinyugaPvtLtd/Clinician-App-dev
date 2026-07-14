fa4cf4bde1e89f05fb27d0694a01036ddbeccbeb
ShubhDev <shubham.dev@symmetry.care>
2026-07-14 16:18:31 +0530

    Validation enhancement

--- lib/core/constant/api_app_constant.dart
+++ lib/core/constant/api_app_constant.dart
@@ -1,34 +1,6 @@
 ///dev
-class ApiAppConstant {
-  static  String domain = 'https://prohealth-dev.symmetry.care/';
-  static const String authDomain = 'https://auth.symmetry.care/';
-  static  String clinicialBaseUrl = '${domain}api/';
-  static const String clinicialAuthBaseUrl = '${authDomain}api/';
-  static const googleApiKey = "AIzaSyCw6mXOPCtbKn5i0bWcAcnfXCkb0y5G7Lg";
-
-  static const int proHealth = 2;
-  static const int clinician = 1;
-  //  pass appType to get dynamic base URL
-  static String endPointByAlias(int appType,String aliasString) {
-    switch (appType) {
-      case clinician:
-        domain = "https://prohealth-dev.symmetry.care";
-        return "https://prohealth-dev.symmetry.care";
-      case proHealth:
-        domain = "https://prohealth-dev.symmetry.care";
-        return "https://prohealth-dev.symmetry.care";
-      default:
-        domain = "https://prohealth-dev.symmetry.care/";
-        return "https://prohealth-dev.symmetry.care/";
-    }
-  }
-}
-
-
-
-///demo
 // class ApiAppConstant {
-//   static  String domain = 'https://demo.symmetry.care/';
+//   static  String domain = 'https://prohealth-dev.symmetry.care/';
 //   static const String authDomain = 'https://auth.symmetry.care/';
 //   static  String clinicialBaseUrl = '${domain}api/';
 //   static const String clinicialAuthBaseUrl = '${authDomain}api/';
@@ -40,16 +12,44 @@ class ApiAppConstant {
 //   static String endPointByAlias(int appType,String aliasString) {
 //     switch (appType) {
 //       case clinician:
-//         domain = "https://demo.symmetry.care";
-//         return "https://demo.symmetry.care";
+//         domain = "https://prohealth-dev.symmetry.care";
+//         return "https://prohealth-dev.symmetry.care";
 //       case proHealth:
-//         domain = "https://demo.symmetry.care";
-//         return "https://demo.symmetry.care";
+//         domain = "https://prohealth-dev.symmetry.care";
+//         return "https://prohealth-dev.symmetry.care";
 //       default:
-//         domain = "https://demo.symmetry.care/";
-//         return "https://demo.symmetry.care/";
+//         domain = "https://prohealth-dev.symmetry.care/";
+//         return "https://prohealth-dev.symmetry.care/";
 //     }
 //   }
 // }
+
+
+
+///demo
+class ApiAppConstant {
+  static  String domain = 'https://demo.symmetry.care/';
+  static const String authDomain = 'https://auth.symmetry.care/';
+  static  String clinicialBaseUrl = '${domain}api/';
+  static const String clinicialAuthBaseUrl = '${authDomain}api/';
+  static const googleApiKey = "AIzaSyCw6mXOPCtbKn5i0bWcAcnfXCkb0y5G7Lg";
+
+  static const int proHealth = 2;
+  static const int clinician = 1;
+  //  pass appType to get dynamic base URL
+  static String endPointByAlias(int appType,String aliasString) {
+    switch (appType) {
+      case clinician:
+        domain = "https://demo.symmetry.care";
+        return "https://demo.symmetry.care";
+      case proHealth:
+        domain = "https://demo.symmetry.care";
+        return "https://demo.symmetry.care";
+      default:
+        domain = "https://demo.symmetry.care/";
+        return "https://demo.symmetry.care/";
+    }
+  }
+}
 
 
