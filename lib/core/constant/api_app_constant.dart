51bfc45a89d2b5db8eef3c5e05974c5ee817a1fc
ShubhDev <shubham.dev@symmetry.care>
2026-07-27 15:36:47 +0530

    den-final

--- lib/core/constant/api_app_constant.dart
+++ lib/core/constant/api_app_constant.dart
@@ -28,7 +28,7 @@
 
 ///demo
 class ApiAppConstant {
-  static  String domain = 'https://demo.symmetry.care/';
+  static  String domain = 'https://prohealth-dev.symmetry.care/';
   static const String authDomain = 'https://auth.symmetry.care/';
   static  String clinicialBaseUrl = '${domain}api/';
   static const String clinicialAuthBaseUrl = '${authDomain}api/';
@@ -40,14 +40,14 @@ class ApiAppConstant {
   static String endPointByAlias(int appType,String aliasString) {
     switch (appType) {
       case clinician:
-        domain = "https://demo.symmetry.care";
-        return "https://demo.symmetry.care";
+        domain = "https://prohealth-dev.symmetry.care";
+        return "https://prohealth-dev.symmetry.care";
       case proHealth:
-        domain = "https://demo.symmetry.care";
-        return "https://demo.symmetry.care";
+        domain = "https://prohealth-dev.symmetry.care";
+        return "https://prohealth-dev.symmetry.care";
       default:
-        domain = "https://demo.symmetry.care/";
-        return "https://demo.symmetry.care/";
+        domain = "https://prohealth-dev.symmetry.care/";
+        return "https://prohealth-dev.symmetry.care/";
     }
   }
 }
